2bcca41651205b3e7e297c7f5eecef0c38838c12
coreyffa <scruggscorey48@gmail.com>
2014-11-04 22:49:18 -0500

    Updated to 1.7.10

--- src/main/java/com/gamerlord48/crackfox/help/Reference.java
+++ src/main/java/com/gamerlord48/crackfox/help/Reference.java
@@ -7,5 +7,5 @@ public class Reference {
     them once here, and we can always access them.
      */
     public static final String MODID = "crackfox";
-    public static final String VERSION = "1.7.2-0.1";
+    public static final String VERSION = "1.7.10-0.1";
 }
