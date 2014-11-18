562f4b92cd60028a79c385ff6d0143443ad62fee
coreyffa <scruggscorey48@gmail.com>
2014-11-17 20:14:12 -0500

    Worked on food item some more, fixed CrackFoxBlock texture

--- src/main/java/com/gamerlord48/crackfox/blocks/CrackFoxBlock.java
+++ src/main/java/com/gamerlord48/crackfox/blocks/CrackFoxBlock.java
@@ -3,53 +3,27 @@ package com.gamerlord48.crackfox.blocks;
 
 import com.gamerlord48.crackfox.CrackFox;
 import com.gamerlord48.crackfox.help.Reference;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.util.IIcon;
 
 
 public class CrackFoxBlock extends Block {
 
-    @SideOnly(Side.CLIENT)
-    private IIcon top;
-    @SideOnly(Side.CLIENT)
-    private IIcon front;
-    @SideOnly(Side.CLIENT)
-    private IIcon back;
-    @SideOnly(Side.CLIENT)
-    private IIcon face1;
-    @SideOnly(Side.CLIENT)
-    private IIcon face2;
-    @SideOnly(Side.CLIENT)
-    private IIcon face3;
-    @SideOnly(Side.CLIENT)
-    private IIcon bottom;
-
     public CrackFoxBlock() {
         super(Material.wood);
         setBlockName("crackFoxBlock");
-        setBlockTextureName(Reference.MODID + ":" + "crackFoxBlock");
+        setBlockTextureName(Reference.MODID + ":" + getUnlocalizedName().substring(5));
         setCreativeTab(CrackFox.tabCrackFox);
         setStepSound(soundTypeWood);
         setHardness(1.0F);
-        setHarvestLevel("axe", 1);
+        setHarvestLevel("any", 1);
         setLightLevel(1.0F);
     }
 
-    @SideOnly(Side.CLIENT)
-    public IIcon getIcon(int side, int meta)
+    //return the metadata of the block when it is dropped
+    @Override
+    public int damageDropped(int metadata)
     {
-        return side == 1 || side == 0 ? this.top : (side == 2 ? this.front : this.blockIcon);
-    }
-
-    @SideOnly(Side.CLIENT)
-    public void registerBlockIcons(IIconRegister p_149651_1_)
-    {
-        this.blockIcon = p_149651_1_.registerIcon(getTextureName() + "_side");
-        this.top = p_149651_1_.registerIcon(getTextureName() + "_top");
-        this.front = p_149651_1_.registerIcon(getTextureName() + "_front");
+        return metadata;
     }
 }
--- src/main/java/com/gamerlord48/crackfox/items/ModItems.java
+++ src/main/java/com/gamerlord48/crackfox/items/ModItems.java
@@ -1,7 +1,16 @@
 package com.gamerlord48.crackfox.items;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+
 public class ModItems {
     //non-functional items.
+
+    public static Item testFood;
+
     //public static Item chromiumNugget;
 
     //Tools
@@ -19,6 +28,10 @@ public class ModItems {
      */
 
     public static void loadItems() {
+        ItemFood testFood;
+        GameRegistry.registerItem(testFood =
+                new ItemTestFood("testFood", 2, 0.2f, false,
+                        new PotionEffect(Potion.damageBoost.id, 1000, 1)).setAlwaysEdible(), "testFood");
         /**
         chromiumNugget = new ItemChromiumNugget();
 
