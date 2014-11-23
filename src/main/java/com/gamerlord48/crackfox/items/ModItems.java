d031f9dfdaad29a562be3a25bbc3144aaa9a8a5a
coreyffa <scruggscorey48@gmail.com>
2014-11-23 06:54:12 -0500

    Removed all WIP features from master branch to comply with new branching methods

--- src/main/java/com/gamerlord48/crackfox/items/ModItems.java
+++ src/main/java/com/gamerlord48/crackfox/items/ModItems.java
@@ -2,16 +2,10 @@ package com.gamerlord48.crackfox.items;
 
 import com.gamerlord48.crackfox.help.RegisterHelper;
 import net.minecraft.item.Item;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.common.util.EnumHelper;
 
 
 public class ModItems {
-
-    //Food Items
-    public static Item testFood;
-    public static Item poisonFood;
 
     //non-functional items.
     public static Item chromiumNugget;
@@ -34,13 +28,9 @@ public class ModItems {
 
     public static void loadItems() {
 
-        testFood =new ItemTestFood("testFood", 2, 0.2f, false, new PotionEffect(Potion.damageBoost.id, 1000, 1)).setAlwaysEdible();
-
-        poisonFood = new ItemPoisonFood("poisonFood", 2, 0.2f, false, new PotionEffect(Potion.poison.id, 100, 1)).setAlwaysEdible();
-
+        //Non Functional Items
         chromiumNugget = new ItemChromiumNugget();
 
-        RegisterHelper.registerItem(chromiumNugget);
 
         //Tools
         chromiumPickaxe = new ItemChromiumPickaxe(chromiumToolMaterial);
@@ -51,7 +41,6 @@ public class ModItems {
         RegisterHelper.registerItem(chromiumAxe);
         RegisterHelper.registerItem(chromiumSpade);
 
-        RegisterHelper.registerItem(poisonFood);
-        RegisterHelper.registerItem(testFood);
+        RegisterHelper.registerItem(chromiumNugget);
     }
 }
