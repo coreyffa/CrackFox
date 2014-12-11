86ffb2a91b18d24631507e7e66c18fdcdc3e7981
coreyffa <scruggscorey48@gmail.com>
2014-12-11 00:54:45 -0500

    Changed chromium material harvest level to iron

--- src/main/java/com/gamerlord48/crackfox/items/ModItems.java
+++ src/main/java/com/gamerlord48/crackfox/items/ModItems.java
@@ -26,7 +26,7 @@ public class ModItems {
     // Armor
 
     //materials
-    static Item.ToolMaterial chromiumToolMaterial = EnumHelper.addToolMaterial("chromiumToolMaterial", 1, 500, 10.0F, 4.0F, 20);
+    static Item.ToolMaterial chromiumToolMaterial = EnumHelper.addToolMaterial("chromiumToolMaterial", 2, 500, 10.0F, 4.0F, 20);
 
     /**
      * We call this in our main mod file.
